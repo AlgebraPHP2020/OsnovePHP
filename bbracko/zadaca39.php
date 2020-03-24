0ae004741575d82d4f64e686d24ebf02e9be502b
bbracko <brankabracko@gmail.com>
2020-03-24 17:40:25 +0100

    moja zadaca

--- bbracko/zadaca39.php
+++ bbracko/zadaca39.php
@@ -1,8 +1,46 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* ovo je moja zadaca
+ * zadatak broj 2. */
+ 
+$ime="Branka";
+$prezime="Bračko";
+echo $ime." ".$prezime;
+echo "\n";
+
+/* zadatak broj 3. */
+
+$a=5;
+$a++;
+echo $a;
+echo "\n";
+$a--;
+echo $a;
+echo "\n";
+
+/* zadarak broj 4. */
+
+$a=(float)$a;
+echo $a."--> ";
+echo gettype ($a)."\n";
+
+
+/* zadatak broj 5. */
+
+//$a=$a+5;
+$a+=5;
+echo $a;
+echo "\n";
+
+/* zadatak broj 6. */
+
+$a=7;
+$b=3;
+
+echo $a."+".$b."=".($a+$b)."\n";
+echo $a."-".$b."=".($a-$b)."\n";
+echo $a."*".$b."=".($a*$b)."\n";
+echo $a."/".$b."=".($a/$b)."\n";
+echo $a."%".$b."=".($a%$b)."\n";
+
 
