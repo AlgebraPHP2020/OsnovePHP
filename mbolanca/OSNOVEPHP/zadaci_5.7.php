b14994ebbcab7d01fa973e751e1685713727cd98
bmartin86 <iMuy11pMpk>
2020-03-28 19:57:36 +0100

    3.zadatak

--- mbolanca/OSNOVEPHP/zadaci_5.7.php
+++ mbolanca/OSNOVEPHP/zadaci_5.7.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<strong>1. Koristeći se operatorima usporedbe &nbsp < &nbsp i &nbsp >&nbsp , te iskazom if-else <br>'
+echo '<strong>1. Koristeći se operatorima usporedbe &nbsp < &nbsp i &nbsp >&nbsp , te iskazom <i>if-else</i> <br>'
 . 'provjeri odnos varijabli &nbsp $a = 4 i &nbsp $b = 2 i ispiši poruku.</strong><br><br>';
 
 $a = 7;
@@ -14,8 +14,8 @@ if ($a>$b){
     echo 'Varijabla $a = '.$a.' jednaka je varijabli $b = '.$b;
 }
 
-echo '<br><br><strong>2. Napiši program koji rabi dvije varijable $a i $b. Ako je $a veće od $b <br>'
-. 'uvećaj $a za 1, a ako je veći $b onda ga umanji za 1. Na kraju ispiši vrijednost varijabli.</strong><br><br>';
+echo '<br><br><strong>2. Napiši program koji rabi dvije varijable $a i $b. Ako je $a veće od $b, uvećaj ga za 1, <br>'
+. 'a ako je veći $b onda ga umanji za 1. Na kraju ispiši vrijednost varijabli.</strong><br><br>';
 
 $a = 17;
 $b = 4;
@@ -25,12 +25,12 @@ $b = 4;
 if ($a>$b){
     
     $a++;
-    echo ' $a je veći od $b pa je uvećan za jedan i sad iznosi ' . $a;
+    echo ' $a je veći od $b pa je uvećan. <br> $a = ' . $a;
     
 } else if ($a<$b) {
     
     $b--;
-    echo ' $b je veći od $a pa je umanjen za jedan i sad iznosi ' . $b;
+    echo ' $b je veći od $a pa je umanjen. <br> $b = ' . $b;
     
 } else {
     
@@ -38,3 +38,13 @@ if ($a>$b){
     
 }
 
+echo "<br><br><strong>3. Pomoću petlje <i>while</i> ispiši brojeve od 1 do 50, jedan ispod drugoga.</strong><br><br>";
+
+$a = 1;
+
+while ($a<=50) {
+    
+    echo $a ."<br>";
+    $a++;
+    
+}
