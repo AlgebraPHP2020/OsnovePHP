d20e2f68abd5dbc548f88a0f291b6602344182ba
Marko Maravić <markomaravic1996@gmail.com>
2020-03-30 20:52:01 +0200

    probni novi php

---
+++ jsperkovic/newEmptyPHP.php
@@ -0,0 +1,12 @@
+<?php
+
+ $a = 23;
+        $b = 12;
+        
+        if ($a > $b) {
+            $a++;
+            echo '$a je veće';
+        } else {
+            $b--;
+            echo '$b je veće'; 
+        }
---
+++ newEmptyPHP1.php
@@ -0,0 +1,58 @@
+<?php
+
+$ime="Marko";
+$prezime="Maravić";
+
+echo "Moje ime je " .$ime. " ".$prezime;  
+
+echo "<br>";
+
+$a="5";
+$a++;
+
+echo $a;
+
+echo "<br>";
+
+$a--;
+
+echo $a;
+
+echo "<br>";
+
+$b=(float) $a;
+
+echo $b;
+
+echo "<br>";
+
+$a += "5";
+
+echo $a;
+
+echo "<br>";
+
+$a="7";
+$b="3";
+
+echo ($a + $b) . "<br>";
+echo ($a - $b) . "<br>";
+echo ($a * $b) . "<br>";
+echo ($a / $b) . "<br>";
+echo ($a % $b) . "<br>";
+
+$a="4";
+$b="2";
+
+echo ($a + $b) . "<br>";
+echo ($a - $b) . "<br>";
+echo ($a * $b) . "<br>";
+echo ($a / $b) . "<br>";
+echo ($a % $b) . "<br>";
+
+
+        
+        
+
+
+
